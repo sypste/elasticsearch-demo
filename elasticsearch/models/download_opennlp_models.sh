fc092126f1f221cadf2e1575ae5036b495d3e91d
Simon <simon.stemper@pm.me>
2022-09-09 16:04:04 +0000

    correct dir for models

---
+++ elasticsearch/models/download_opennlp_models.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+curl http://opennlp.sourceforge.net/models-1.5/en-ner-person.bin --output en-ner-persons.bin
+curl http://opennlp.sourceforge.net/models-1.5/en-ner-location.bin --output en-ner-locations.bin
+curl http://opennlp.sourceforge.net/models-1.5/en-ner-date.bin --output en-ner-dates.bin 
